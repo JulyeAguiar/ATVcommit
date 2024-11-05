329f917d8140c3b0d945bb50f05e09bac9896caf
JulyeAguiar <131816932+JulyeAguiar@users.noreply.github.com>
2024-11-05 09:50:54 -0400

    Destaque
    A classe para destacar um produto foi adicionada

--- arquivoteste.js
+++ arquivoteste.js
@@ -1,12 +1,12 @@
 class Errocometido extends Error {
-    constructor(mensage){
+    constructor(mensage) {
         super(mensage);
         this.name = "Erro: está faltando adicionar os atributos";
     }
 }
 
-class Produto{
-    constructor(nome, dataCadastro, descricao, preco){
+class Produto {
+    constructor(nome, dataCadastro, descricao, preco) {
         this.nome = nome;
         this.dataCadastro = dataCadastro;
         this.descricao = descricao;
@@ -14,16 +14,16 @@ class Produto{
     }
 
     mostrarProduto() {
-        try{
-          return this.atributos()
-        } catch(erro){
-          console.log(erro)
+        try {
+            return this.atributos()
+        } catch (erro) {
+            console.log(erro)
         }
-      }
+    }
 
-      atributos() {
+    atributos() {
         if (this.nome != "" && this.dataCadastro != "" && this.descricao != "" && this.preco != "") {
-          return `
+            return `
           <div class="produto">
               <h2>${this.nome}</h2>
               <p>${this.descricao}</p>
@@ -31,9 +31,42 @@ class Produto{
               <div class="cadastro">Cadastrado em: ${this.dataCadastro}</div>
           </div>
               `
-           } else {
+        } else {
             throw new Errocometido("Está faltando inserir alguma das informações necessárias para elaborar o produto :(")
-          }
-      }
-     
+        }
+    }
+
+}
+
+
+class destaque extends Produto {
+    constructor(nome, dataCadastro, descricao, preco, imagem) {
+        super(nome, dataCadastro, descricao, preco)
+        this.imagem = imagem
+    }
+
+    mostrarDestaque() {
+        try {
+            return this.imgdestaque()
+        } catch (erro) {
+            console.log(erro)
+        }
+    }
+
+    imgdestaque() {
+        if (this.nome != "" && this.dataCadastro != "" && this.descricao != "" && this.preco != "" && this.imagem != "") {
+            return `
+          <div class="pDestaque">
+            <img class="imgDestaque" src="${this.imagem}"></img>
+            <h2>${this.nome}</h2>
+            <p>${this.descricao}</p>
+            <div class="precoDestaque">R$${this.preco},00</div>
+            <div class="cadastroDestaque">Cadastrado em: ${this.dataCadastro}</div>
+          </div>
+              `
+        } else {
+            throw new Errocometido("Está faltando inserir alguma das informações necessárias para elaborar o produto :(")
+        }
+    }
+
 }
